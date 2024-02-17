3fa8d45295293fd4ee2d19ccecbb96c2513be0a1
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-02-17 15:42:58 -0300

    feat: add Header and Nav Component and Constants Links

---
+++ src/components/constants/navigation.ts
@@ -0,0 +1,22 @@
+export const links = {
+  navigation: [
+    {
+      name: 'Sobre',
+      href: '/me'
+    },
+    {
+      name: 'Projetos',
+      href: '/projects'
+    },
+    {
+      name: 'Contato',
+      href: '/contact'
+    }
+  ],
+  social: [
+    { name: 'Instagram', href: '/' },
+    { name: 'Linkedin', href: '/' },
+    { name: 'Twitter', href: '/' },
+    { name: 'GitHub', href: '/' }
+  ]
+};
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,29 +1,31 @@
 import { ExternalLink } from 'lucide-react';
+import { links } from '../constants/navigation';
+
+type NavLinksProps = {
+  name: string;
+  href: string;
+};
+
+const NavLinksList = ({ links }: { links: NavLinksProps[] }) => {
+  return (
+    <ul>
+      {links.map((link) => (
+        <li key={link.name}>{link.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+const Nav = () => {
+  return (
+    <nav className="flex items-start gap-40">
+      <NavLinksList links={links.navigation} />
+      <NavLinksList links={links.social} />
+    </nav>
+  );
+};
 
 const Header = () => {
-  const links = {
-    navigation: [
-      {
-        name: 'Sobre',
-        href: '/me'
-      },
-      {
-        name: 'Projetos',
-        href: '/projects'
-      },
-      {
-        name: 'Contato',
-        href: '/contact'
-      }
-    ],
-    social: [
-      { name: 'Instagram', href: '/' },
-      { name: 'Linkedin', href: '/' },
-      { name: 'Twitter', href: '/' },
-      { name: 'GitHub', href: '/' }
-    ]
-  };
-
   return (
     <header className="h-[14vh] w-full">
       <div className="flex w-full items-start justify-between p-12 text-sm">
@@ -36,26 +38,7 @@ const Header = () => {
             Blog <ExternalLink size={14} />
           </a>
         </div>
-        <nav className="flex items-start gap-40">
-          <div>
-            <ul className="text-sm">
-              {links.navigation.map((link) => (
-                <li key={`Navegue em ${link.name}`}>
-                  <a href={link.href}>{link.name}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <ul className="text-sm">
-              {links.social.map((link) => (
-                <li key={`Veja meu perfil em ${link.name}`}>
-                  <a href={link.href}>{link.name}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </nav>
+        <Nav />
       </div>
     </header>
   );
